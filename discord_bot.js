9a6dd0cb7407e5ad265444be1a57017270918a30
Kenny Y <46588600+kennyy6@users.noreply.github.com>
2020-02-05 20:10:23 +1100

    in progress send images

--- discord_bot.js
+++ discord_bot.js
@@ -1,13 +1,20 @@
-const DISCORD = require("discord.js");
+const {DISCORD, Attachment} = require("discord.js");
 
 
-const bot = new DISCORD.Client();
+const bot = new Client();
 
 
 // check the msgs sent
 bot.on('message',(message)=>{
-    if (message.content == 'ping'){
-        message.reply("pong");
+    var firstElemString = message.content.charAt(0);
+
+    if (firstElemString == '!'){
+
+
+        var attachment = new Attachment("http://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV09-5lpKKqP_xMq3I2D0B65Jy2b7FrNqgjQXn8hVvazj3J4OdIFRsY1nYrgDtxO27h8W5uZvXiSw07ljG5dE")
+
+        message.channel.send(message.author,attachment);
+        //message.reply("pong");
         //message.channel.sendMessage(message.content);
     }
 
