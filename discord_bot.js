6e1adb278847edeaa056173672fece696afeda3d
Kenny Y <46588600+kennyy6@users.noreply.github.com>
2020-02-11 23:00:07 +1100

    finished csgo item api

--- discord_bot.js
+++ discord_bot.js
@@ -1,6 +1,7 @@
 //const {DISCORD, Attachment} = require("discord.js");
 const Discord = require('discord.js');
-const axios = require('axios');
+const axios = require("axios");
+
 const {Client, Attachment} = require('discord.js');
 const bot = new Client();
 
@@ -11,9 +12,9 @@ const bot = new Client();
 
 
 // check the msgs sent
-bot.on('message', async (message)=>{
+bot.on('message', async message=>{
     var firstElemString = message.content.charAt(0);
-    message.reply(message.content);
+    //message.reply(message.content);
         //if (firstElemString == '!'){
 
 
@@ -28,25 +29,55 @@ bot.on('message', async (message)=>{
         
 
         // testAPI();} 
+    if (message.content == "!example" || message.content == "!help"){
+        message.reply("!skins M4A4 | Asiimov (Field-Tested) is a command you should try ")
+    }
 
     if (message.content == '!joke'){
         let getJoke = async () =>{
             let response = await axios.get("https://official-joke-api.appspot.com/random_joke"); // await will wait till variables get sent
+             //console.log(response.data);
+            
             let joke = response.data;
             return joke
         }
         let jokeValue = await getJoke();
-        message.reply(jokeValue);
+        //console.log(jokeValue);
+        message.reply(`Here is your joke:  \n  ${jokeValue.setup}\n ${jokeValue.punchline}` );
     }
+    let skins_search = message.content.substring(0,6);
+    if (skins_search == '!skins'){
+        let skin_name =  message.content.substring(7,message.content.length);
+        let baseURI = "https://steamcommunity.com/market/priceoverview/?currency=1&appid=730&market_hash_name=";
+        skin_nameEncode = encodeURI(skin_name);
+
+        let getPrice = async()=>{
+            //console.log(baseURI+skin_name);
+            let priceresponse = await axios.get(baseURI+skin_nameEncode);
+            return priceresponse.data;
+        }
+        let flag = true;
+        try{
+        let priceValue = await getPrice();
+        //console.log(flag);
+        message.reply(`"The item requested: ${skin_name} \n Lowest Price ${priceValue.lowest_price}\n Volume: ${priceValue.volume}`);
+        }
+        catch(e){
+        message.reply("Could not find the price");
+        //console.log(e);
+        let flag = false;
+
+        }
+        finally{
+
+            }
+
+        
+    }
+
 
 });
 
-function testAPI(){
-    // loadJSON("https://steamcommunity.com/market/priceoverview/?currency=1&appid=730&market_hash_name=M4A4%20%7C%20Tornado%20%28Well-Worn%29",funreply)
-}
-function funreply(data){
-    message.reply(data);
-}
 
 
 bot.login('NjcwOTMwMDc5NjcyNTY1Nzcw.Xi1jGA._QWeyaEenM1oZgFiEkkXRHCy1A4');
